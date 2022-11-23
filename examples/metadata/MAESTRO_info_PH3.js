6b4b617431d57517800f07c82809271bc293e487
Natasha Batalha <natasha.e.batalha@gmail.com>
2022-11-22 16:55:03 -0800

    ph3

---
+++ examples/metadata/MAESTRO_info_PH3.js
@@ -0,0 +1,55 @@
+{"calculation": {
+        "id": "PH3",
+        "mol": "PH3",
+        "iso_mol": "31P-1H3",
+        "iso_abundance": "1",
+        "bibid_iso_abundance":"\\cite{INSERT}",
+        "ptid": "REPLACE",
+        "temperature": "REPLACE",
+        "pressure": "REPLACE",
+        "intensity_cutoff": 0,
+        "wing_cutoff": "P$>$1000 bar: 1000cm$^{-1}$, P$>$50 bar & $<$1000 bar: 100cm$^{-1}$, P$\\le$50 bar: 30cm$^{-1}$",
+        "line_profile": "\\cite{10.1016/j.jqsrt.2021.107949}", 
+        "code": "\\cite{10.1016/j.jqsrt.2016.03.005}",
+        "start_wno": "REPLACE",
+        "delta_wno": "REPLACE",
+        "num_bins": "REPLACE",
+        "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}"
+    },
+        "broadener_He": {
+            "broadened_by": "He",
+            "abundance":  0.15,
+            "calculation_type": "P_bro(J)",
+            "functional_form": "",
+            "inputs": "",
+            "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}",
+            "usr_warning": 0,
+            "usr_note": "",
+            "todo": ""
+    },
+
+
+        "broadener_H2": {
+            "broadened_by": "H2",
+            "abundance": 0.85,
+            "calculation_type": "P_bro(J)",
+            "functional_form": "",
+            "inputs": "",
+            "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}",
+            "usr_warning": 0,
+            "usr_note": "",
+            "todo": ""
+    },
+
+        "linelist": {
+            "wno_min": 30.0717909,
+            "wno_max":  3659.265700,
+            "temp_min": 75,
+            "temp_max":  300,
+            "db_name": "HITRAN2020",
+            "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}",
+            "usr_warning": 0,
+            "usr_note": "",
+            "todo": ""
+    }
+}
