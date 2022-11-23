8a9a3fe37589e54c5cbc0ca1fabae3b033660889
Natasha Batalha <natasha.e.batalha@gmail.com>
2022-11-22 16:42:10 -0800

    add examples folder

---
+++ examples/metadata/MAESTRO_info_C2H2.js
@@ -0,0 +1,234 @@
+{"C2H2":
+    {"calculation": {
+        "id": "C2H2",
+        "mol": "C2H2",
+        "iso_mol": "C2H2",
+        "iso_abundance": "1",
+        "bibid_iso_abundance":"\\cite{INSERT}",
+        "ptid": "REPLACE",
+        "temperature": "REPLACE",
+        "pressure": "REPLACE",
+        "intensity_cutoff": 0,
+        "wing_cutoff": "P$>$1000 bar: 1000cm$^{-1}$, P$>$50 bar & $<$1000 bar: 100cm$^{-1}$, P$\\le$50 bar: 30cm$^{-1}$",
+        "line_profile": "\\cite{10.1016/j.jqsrt.2021.107949}", 
+        "code": "\\cite{10.1016/j.jqsrt.2016.03.005}",
+        "start_wno": "REPLACE",
+        "delta_wno": "REPLACE",
+        "num_bins": "REPLACE",
+        "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}"
+    },
+        "broadener_He": {
+            "broadened_by": "He",
+            "abundance":  0.15,
+            "calculation_type": "P_bro(J)",
+            "functional_form": "",
+            "inputs": "",
+            "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}",
+            "usr_warning": 0,
+            "usr_note": "",
+            "todo": ""
+    },
+
+
+        "broadener_H2": {
+            "broadened_by": "H2",
+            "abundance": 0.85,
+            "calculation_type": "P_bro(J)",
+            "functional_form": "",
+            "inputs": "",
+            "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}",
+            "usr_warning": 0,
+            "usr_note": "",
+            "todo": ""
+    },
+
+        "linelist": {
+            "wno_min": 30.061830,
+            "wno_max":  10736.234900,
+            "temp_min": 75,
+            "temp_max":  300,
+            "db_name": "HITRAN2020",
+            "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}",
+            "usr_warning": 0,
+            "usr_note": "",
+            "todo": ""
+    }
+    },
+},
+
+
+{"12C2-1H2":
+    {"calculation": {
+        "id": "C2H2",
+        "mol": "C2H2",
+        "iso_mol": "12C2-1H2",
+        "iso_abundance": "0.977599",
+        "bibid_iso_abundance":"\\cite{INSERT}",
+        "ptid": "REPLACE",
+        "temperature": "REPLACE",
+        "pressure": "REPLACE",
+        "intensity_cutoff": 0,
+        "wing_cutoff": "P$>$1000 bar: 1000cm$^{-1}$, P$>$50 bar & $<$1000 bar: 100cm$^{-1}$, P$\\le$50 bar: 30cm$^{-1}$",
+        "line_profile": "\\cite{10.1016/j.jqsrt.2021.107949}", 
+        "code": "\\cite{10.1016/j.jqsrt.2016.03.005}",
+        "start_wno": "REPLACE",
+        "delta_wno": "REPLACE",
+        "num_bins": "REPLACE",
+        "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}"
+    },
+        "broadener_He": {
+            "broadened_by": "He",
+            "abundance":  0.15,
+            "calculation_type": "P_bro(J)",
+            "functional_form": "",
+            "inputs": "",
+            "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}",
+            "usr_warning": 0,
+            "usr_note": "",
+            "todo": ""
+    },
+
+
+        "broadener_H2": {
+            "broadened_by": "H2",
+            "abundance": 0.85,
+            "calculation_type": "P_bro(J)",
+            "functional_form": "",
+            "inputs": "",
+            "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}",
+            "usr_warning": 0,
+            "usr_note": "",
+            "todo": ""
+    },
+
+        "linelist": {
+            "wno_min": 30.061830,
+            "wno_max":  10736.234900,
+            "temp_min": 75,
+            "temp_max":  300,
+            "db_name": "HITRAN2020",
+            "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}",
+            "usr_warning": 0,
+            "usr_note": "",
+            "todo": ""
+    }
+    },
+},
+
+
+{"1H-12C-13C-1H":
+    {"calculation": {
+        "id": "C2H2",
+        "mol": "C2H2",
+        "iso_mol": "1H-12C-13C-1H",
+        "iso_abundance": "0.021966",
+        "bibid_iso_abundance":"\\cite{INSERT}",
+        "ptid": "REPLACE",
+        "temperature": "REPLACE",
+        "pressure": "REPLACE",
+        "intensity_cutoff": 0,
+        "wing_cutoff": "P$>$1000 bar: 1000cm$^{-1}$, P$>$50 bar & $<$1000 bar: 100cm$^{-1}$, P$\\le$50 bar: 30cm$^{-1}$",
+        "line_profile": "\\cite{10.1016/j.jqsrt.2021.107949}", 
+        "code": "\\cite{10.1016/j.jqsrt.2016.03.005}",
+        "start_wno": "REPLACE",
+        "delta_wno": "REPLACE",
+        "num_bins": "REPLACE",
+        "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}"
+    },
+        "broadener_He": {
+            "broadened_by": "He",
+            "abundance":  0.15,
+            "calculation_type": "P_bro(J)",
+            "functional_form": "",
+            "inputs": "",
+            "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}",
+            "usr_warning": 0,
+            "usr_note": "",
+            "todo": ""
+    },
+
+
+        "broadener_H2": {
+            "broadened_by": "H2",
+            "abundance": 0.85,
+            "calculation_type": "P_bro(J)",
+            "functional_form": "",
+            "inputs": "",
+            "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}",
+            "usr_warning": 0,
+            "usr_note": "",
+            "todo": ""
+    },
+
+        "linelist": {
+            "wno_min": 613.536460,
+            "wno_max":  9856.610400,
+            "temp_min": 75,
+            "temp_max":  300,
+            "db_name": "HITRAN2020",
+            "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}",
+            "usr_warning": 0,
+            "usr_note": "",
+            "todo": ""
+    }
+    },
+},
+
+
+{"1H-12C-12C-2D":
+    {"calculation": {
+        "id": "C2H2",
+        "mol": "C2H2",
+        "iso_mol": "1H-12C-12C-2D",
+        "iso_abundance": "3.045500e-4",
+        "bibid_iso_abundance":"\\cite{INSERT}",
+        "ptid": "REPLACE",
+        "temperature": "REPLACE",
+        "pressure": "REPLACE",
+        "intensity_cutoff": 0,
+        "wing_cutoff": "P$>$1000 bar: 1000cm$^{-1}$, P$>$50 bar & $<$1000 bar: 100cm$^{-1}$, P$\\le$50 bar: 30cm$^{-1}$",
+        "line_profile": "\\cite{10.1016/j.jqsrt.2021.107949}", 
+        "code": "\\cite{10.1016/j.jqsrt.2016.03.005}",
+        "start_wno": "REPLACE",
+        "delta_wno": "REPLACE",
+        "num_bins": "REPLACE",
+        "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}"
+    },
+        "broadener_He": {
+            "broadened_by": "He",
+            "abundance":  0.15,
+            "calculation_type": "P_bro(J)",
+            "functional_form": "",
+            "inputs": "",
+            "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}",
+            "usr_warning": 0,
+            "usr_note": "",
+            "todo": ""
+    },
+
+
+        "broadener_H2": {
+            "broadened_by": "H2",
+            "abundance": 0.85,
+            "calculation_type": "P_bro(J)",
+            "functional_form": "",
+            "inputs": "",
+            "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}",
+            "usr_warning": 0,
+            "usr_note": "",
+            "todo": ""
+    },
+
+        "linelist": {
+            "wno_min": 31.710291,
+            "wno_max":  788.717600,
+            "temp_min": 75,
+            "temp_max":  300,
+            "db_name": "HITRAN2020",
+            "bibid": "\\cite{10.1016/j.jqsrt.2021.107949}",
+            "usr_warning": 0,
+            "usr_note": "",
+            "todo": ""
+    }
+    },
+}
